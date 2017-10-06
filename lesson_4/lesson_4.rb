96d8f11d1604997f52a02509c92c711d3083389f
Vitaliy Ostrovskiy <vitalikostrovskij@yandex.ru>
2017-10-06 12:23:59 +0300

    add lesson 4

---
+++ lesson_4/lesson_4.rb
@@ -0,0 +1,8 @@
+puts 'Урок -4'
+puts "Введите свое Имя:"
+name = gets.chomp
+puts "Введите свою Фамилию:"
+surname = gets.chomp
+puts "Введите свое Отчество:"
+patronymic = gets.chomp
+puts 'Доброго времени суток!' + name +' '+patronymic+' '+surname
