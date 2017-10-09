193e5ca98df8cd2a44ba789ed24d4dfca246e875
Vitaliy Ostrovskiy <vitalikostrovskij@yandex.ru>
2017-10-09 14:10:11 +0300

    add lesson 6

---
+++ lesson_6/lesson_6.rb
@@ -0,0 +1,13 @@
+beer=99
+puts beer.to_s+' бутилок пива на стене'
+99.times do
+	puts 'Одна упала '
+	beer = beer-1
+	if beer == 0 
+		puts 'Вот больше и нет бутилок на стене'
+	else
+		puts 'Осталось '+beer.to_s+' бутилок пива на стене'
+		puts beer.to_s+' бутилок пива на стене'
+	end
+end
+sleep(5)
